b0987ca708a564e7071a7773777a7769a2a9592a
mgechev <mgechev@gmail.com>
2015-01-20 23:10:13 +0200

    Initial commit

---
+++ proxy/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var WebSocketServer = require('ws').Server;
+var wss = new WebSocketServer({ port: 8081 });
+var net = require('net');
+
+wss.on('connection', function (ws) {
+  var client;
+  ws.on('message', function (msg) {
+    msg = JSON.parse(msg);
+    if (msg.type === 'handshake') {
+      client = net.connect(msg.host, msg.port, function () {
+        console.log('Connected');
+      });
+      client.on('data', function (data) {
+        console.log('data');
+        ws.send(data, { binary: true });
+      });
+      client.on('end', function () {
+        console.log('end');
+      });
+    } else {
+      client.send(msg);
+    }
+  });
+});
